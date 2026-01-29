41c32527def6a58269b3be708526a5eac9f05fcf
Soar Qin <soarchin@gmail.com>
2026-01-29 21:31:23 +0800

    restructured project

--- build.bat
+++ build.bat
@@ -3,11 +3,9 @@
 mkdir dist >NUL 2>&1
 mkdir build >NUL 2>&1
 
-cmake -B build -H. -G "Visual Studio 18 2026"
+cmake -B build -Hc -G "Visual Studio 18 2026"
 cmake --build build --config MinSizeRel --target smtc_c
 
 copy /y build\bin\smtc.dll dist\smtc.dll >NUL 2>&1
 
-pushd smtc-now-playing >NUL
-go build -ldflags="-s -w -H windowsgui" -o ../dist/SmtcNowPlaying.exe
-popd >NUL
+go build -ldflags="-s -w -H windowsgui" -o dist/SmtcNowPlaying.exe
