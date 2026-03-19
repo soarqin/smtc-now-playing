102c11e9b27a57e6616ae8c333f88fd6019bdd34
Soar Qin <soarchin@gmail.com>
2026-03-20 03:02:35 +0800

    chore: commit build.bat cmake removal

--- build.bat
+++ build.bat
@@ -1,11 +1,5 @@
 @echo off
 
 mkdir dist >NUL 2>&1
-mkdir build >NUL 2>&1
-
-cmake -B build -Hc -G "Visual Studio 18 2026"
-cmake --build build --config MinSizeRel --target smtc_c
-
-copy /y build\bin\smtc.dll dist\smtc.dll >NUL 2>&1
 
 go build -ldflags="-s -w -H windowsgui" -o dist/SmtcNowPlaying.exe
